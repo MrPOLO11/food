161ed89684d854e5ec94565b384cfe160e467e18
Marcos Rivera <mrivera22@mail.greenriver.edu>
2020-01-22 13:03:12 -0500

    Added Fat-free, created index page and created a home page

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Marcos Rivera
+ * 01-15-2020
+ * 328/dating/index.php
+ */
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require autoload file
+require_once ("vendor/autoload.php");
+
+//Instantiate Fat-Free framework (F3)
+$f3 = Base::instance();
+
+//Define a default route
+$f3->route('GET /', function() {
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+$f3->run();
